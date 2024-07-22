526579b7164feb5984e02bf7b7d716facabf75ce
theskid22 <shrey150436@gmail.com>
2024-07-21 20:05:14 -0400

    made dynamic

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const server = app.listen(8080, () => {
+    console.log(`Server running on port ${server.address().port}`);
+});
+app.use(express.static('public'))
