bd7d1f4604f3903b140797639fb973c493ea52c5
theskid22 <shrey150436@gmail.com>
2024-07-26 13:29:16 -0400

    co m m mm it    me s a s a ge

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const server = app.listen(8080, () => {
     console.log(`Server running on port ${server.address().port}`);
 });
 app.use(express.static('public'))
-app.use(express.static('img')) // I know that you can prob just put it in /public but i dont care 
+console.log("imported public")
+app.use(express.static('img')) // Yes i can keep it in public but i dont care 
+console.log("imported img")
+console.log("static imports finished")
+
