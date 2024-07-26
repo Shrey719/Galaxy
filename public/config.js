88785965d9e1482dbf74a61988959ea78adb0577
theskid22 <shrey150436@gmail.com>
2024-07-26 12:07:45 -0400

    I Give Totalu Good Commit Messages (sorry lol)

--- public/config.js
+++ public/config.js
@@ -1,3 +1,7 @@
 let config = {
-    cloaking: true
+    cloaking: true,
+    ads: false, // ig if ppl want to watch ads they can? ALWAYS disabled by default
+    lock: false, // Password system if someone wants to self host or smth idk
+    winLock: false, // If os is windows, stop working 
+    winCloak: false // If os is windows, cloak
 }
--- server_config.js
+++ server_config.js
@@ -1,3 +1,4 @@
 const PORT = 8080
 const DNS_Server_IP = null // I aint buying one
-
+const BARE_STATIC = "https://tomp.app/" // pov: no bare
+const BARE_LOCATION = "/bare/"
