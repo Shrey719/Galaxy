d9ec5edc89d9d72b87bbf622322c8f4629f52b78
theskid22 <shrey150436@gmail.com>
2024-07-27 21:14:15 -0400

    thingy

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ console.log("imported public")
 app.use(express.static('img')) // Yes i can keep it in public but i dont care 
 console.log("imported img")
 console.log("static imports finished")
-
+// static imports are don i think
