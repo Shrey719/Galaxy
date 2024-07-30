acc20c0af710f6b6a68a12da8fb6eb690d9d357f
theskid22 <shrey150436@gmail.com>
2024-07-30 13:32:14 -0400

    fixed

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const server = app.listen(8080, () => {
     console.log(`Server running on port ${server.address().port}`);
 });
-app.use(express.static('public/pages'))
-console.log("imported assets")
+app.use(express.static('public')) // LIMO YOU FUCKING DUMBASS (wtf man)
+console.log("imported pages")
 app.use(express.static('img')) // Yes i can keep it in public but i dont care 
 console.log("imported img")
 console.log("static imports finished")
