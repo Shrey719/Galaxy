3d48bf078903ac808a3e2bb0b4d4f992d7c5dbd9
theskid22 <164104873+theskid22@users.noreply.github.com>
2024-07-30 13:25:33 -0400

    BRO YOU FORGOT TO UPDATE INDEX.JS

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const server = app.listen(8080, () => {
     console.log(`Server running on port ${server.address().port}`);
 });
-app.use(express.static('public'))
-console.log("imported public")
+app.use(express.static('assets'))
+console.log("imported assets")
 app.use(express.static('img')) // Yes i can keep it in public but i dont care 
 console.log("imported img")
 console.log("static imports finished")
